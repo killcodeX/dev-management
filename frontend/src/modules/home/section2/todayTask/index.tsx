5a9ccb3b9b4e1931fea2f7ffc4f609db6b9e2009
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-04-16 00:04:59 +0530

    today section

--- frontend/src/modules/home/section2/todayTask/index.tsx
+++ frontend/src/modules/home/section2/todayTask/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function TodayTask() {
   return (
-    <div className='col-sm-6'>
-        <div className='card dashboard-card p-4'>Today Task</div>
+    <div className="col-sm-6">
+      <div className="card dashboard-card p-4">
+        <div className="card-header">
+          <h4 className="card-title fw-bold">Today Task</h4>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- frontend/src/modules/home/section2/todoApp/index.tsx
+++ frontend/src/modules/home/section2/todoApp/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BsSliders } from "react-icons/bs";
 
 export default function Todo() {
   return (
-    <div className='col-sm-6'>
-        <div className='card dashboard-card p-4'>todo</div>
+    <div className="col-sm-6">
+      <div className="card dashboard-card p-4">
+        <div className="card-header">
+          <h4 className="card-title fw-bold">Todo List</h4>
+          <div className="card-tootlbar">
+            <BsSliders />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
